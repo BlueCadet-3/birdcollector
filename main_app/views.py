0def303d411548d804c339a59493d2aae11b55b9
BlueCadet-3 <martysilha@gmail.com>
2020-12-16 16:37:39 -0600

    create temporary bogus data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add temporary fake data
+class Bird:
+  def __init__(self, name, kingdom, phylum, class, order, family, genus, species):
+    self.name = name
+    self.kingdom = kingdom
+    self.phylum = phylum
+    self.class = class
+    self.order = order
+    self.family = family
+    self.genus = genus
+    self.species = species
+
+birds = [
+  Bird('Eastern barn owl', 'Animalia', 'Chordata', 'Aves', 'Strigiformes', 'Tytonidae', 'Tyto', 'T. javanica'),
+  Bird('Common raven', 'Animalia', 'Chordata', 'Aves', 'Passeriformes', 'Corvidae', 'Corvus', 'C. corax'),
+  Bird('Common grackle', 'Animalia', 'Chordata', 'Aves', 'Passeriformes', 'Icteridae', 'Quiscalus', 'Q. quiscula'),
+]
+
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
