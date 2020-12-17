c3ee2877466971e319970e7693bd2dfeefea734e
BlueCadet-3 <martysilha@gmail.com>
2020-12-17 15:42:34 -0600

    homepage render, remove bogus dataset

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Add temporary fake data
-class Bird:
-  def __init__(self, name, kingdom, phylum, classrank, order, family, genus, species):
-    self.name = name
-    self.kingdom = kingdom
-    self.phylum = phylum
-    self.classrank = classrank
-    self.order = order
-    self.family = family
-    self.genus = genus
-    self.species = species
-
-birds = [
-  Bird('Eastern barn owl', 'Animalia', 'Chordata', 'Aves', 'Strigiformes', 'Tytonidae', 'Tyto', 'T. javanica'),
-  Bird('Common raven', 'Animalia', 'Chordata', 'Aves', 'Passeriformes', 'Corvidae', 'Corvus', 'C. corax'),
-  Bird('Common grackle', 'Animalia', 'Chordata', 'Aves', 'Passeriformes', 'Icteridae', 'Quiscalus', 'Q. quiscula'),
-]
-
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
