bd2859b078d8ec235f7b20661bd9c98197af1ec9
BlueCadet-3 <martysilha@gmail.com>
2020-12-17 15:48:01 -0600

    create Bird model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Bird(models.Model):
+  name = models.CharField(max_length=50)
+  kingdom = models.CharField(max_length=50)
+  phylum = models.CharField(max_length=50)
+  classrank = models.CharField(max_length=50)
+  order = models.CharField(max_length=50)
+  family = models.CharField(max_length=50)
+  genus = models.CharField(max_length=50)
+  species = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
