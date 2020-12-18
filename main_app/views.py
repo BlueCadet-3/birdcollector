a9f53c76a521866eaa762c16140a584ebecb5163
BlueCadet-3 <martysilha@gmail.com>
2020-12-18 13:23:34 -0600

    update/delete Bird class based view

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
 
 # Create your views here.
@@ -20,4 +20,12 @@ def birds_detail(request, bird_id):
 class BirdCreate(CreateView):
   model = Bird
   fields = '__all__'
+  success_url = '/birds/'
+
+class BirdUpdate(UpdateView):
+  model = Bird
+  fields = '__all__'
+
+class BirdDelete(DeleteView):
+  model = Bird
   success_url = '/birds/'
