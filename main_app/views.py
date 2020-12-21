f00d03353d85a49c99a8a8d23e9f4b79481c2652
BlueCadet-3 <martysilha@gmail.com>
2020-12-21 12:30:20 -0600

    add Location view functions

--- main_app/views.py
+++ main_app/views.py
@@ -17,6 +17,11 @@ def birds_detail(request, bird_id):
   bird = Bird.objects.get(id=bird_id)
   return render(request, 'birds/detail.html', { 'bird': bird })
 
+def assoc_toy(request, bird_id, location_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Bird.objects.get(id=bird_id).locations.add(location_id)
+  return redirect('detail', bird_id=bird_id)
+
 class BirdCreate(CreateView):
   model = Bird
   fields = '__all__'
@@ -29,3 +34,20 @@ class BirdUpdate(UpdateView):
 class BirdDelete(DeleteView):
   model = Bird
   success_url = '/birds/'
+
+class LocationList(ListView):
+  model = Location
+
+class LocationDetail(DetailView):
+  model = Location
+
+class LocationCreate(CreateView):
+  model = Location
+  fields = '__all__'
+
+class LocationUpdate(UpdateView):
+  model = Location
+
+class LocationDelete(DeleteView):
+  model = Location
+  success_url = '/locations/'
